7117c047d8d01de79ec586a7924ca5e9cbd7d1cd
Alexander Persky <info@alexanderpersky.com>
2016-10-22 19:18:09 -0700

    Format code

--- inc/enqueues.php
+++ inc/enqueues.php
@@ -3,14 +3,13 @@
  * Enqueue scripts and styles.
  */
 function fresh_start_scripts() {
-    wp_enqueue_style( 'fresh-start-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'fresh-start-styles', get_template_directory_uri() . '/assets/css/fresh-start.css' );
 
-    wp_enqueue_script( 'fresh-start-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'fresh-start-scripts', get_template_directory_uri() . '/assets/js/fresh-start.js', array(), '', true );
 
-    wp_enqueue_script( 'fresh-start-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-        wp_enqueue_script( 'comment-reply' );
-    }
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
 }
+
 add_action( 'wp_enqueue_scripts', 'fresh_start_scripts' );
--- inc/theme-extras.php
+++ inc/theme-extras.php
@@ -11,49 +11,51 @@
  * Adds custom classes to the array of body classes.
  *
  * @param array $classes Classes for the body element.
+ *
  * @return array
  */
 function fresh_start_body_classes( $classes ) {
 	$classes[] = 'site_body';
 
-    if ( is_home() ) {
-        $classes[] = '-is-home';
-    }
+	if ( is_home() ) {
+		$classes[] = '-is-home';
+	}
 
-    if ( is_front_page() ) {
-        $classes[] = '-is-homepage';
-    }
+	if ( is_front_page() ) {
+		$classes[] = '-is-homepage';
+	}
 
-    if ( is_front_page() && is_home() ) {
-        $classes[] = '-is-blogroll';
-    }
+	if ( is_front_page() && is_home() ) {
+		$classes[] = '-is-blogroll';
+	}
 
-    if ( is_single() ) {
-        $classes[] = '-is-single';
-    }
+	if ( is_single() ) {
+		$classes[] = '-is-single';
+	}
 
-    if ( is_page() ) {
-        $classes[] = '-is-page';
-    }
+	if ( is_page() ) {
+		$classes[] = '-is-page';
+	}
 
-    if ( is_archive() ) {
-        $classes[] = '-is-archive';
-    }
+	if ( is_archive() ) {
+		$classes[] = '-is-archive';
+	}
 
-    if ( is_search() ) {
-        $classes[] = '-is-search-results';
-    }
+	if ( is_search() ) {
+		$classes[] = '-is-search-results';
+	}
 
-    if ( is_404() ) {
-        $classes[] = '-is-404';
-    }
+	if ( is_404() ) {
+		$classes[] = '-is-404';
+	}
 
-    if ( is_child_theme() ) {
-        $classes[] = '-is-child';
-    }
+	if ( is_child_theme() ) {
+		$classes[] = '-is-child';
+	}
 
 	return $classes;
 }
+
 add_filter( 'body_class', 'fresh_start_body_classes' );
 
 // Remove all body classes except for those inside $whitelist
@@ -71,71 +73,76 @@ function fresh_start_pingback_header() {
 		echo '<link rel="pingback" href="', bloginfo( 'pingback_url' ), '">';
 	}
 }
+
 add_action( 'wp_head', 'fresh_start_pingback_header' );
 
 // Add custom image sizes for this theme
 if ( function_exists( 'add_image_size' ) ) {
-    add_image_size( 'hero', 1500, 9999 ); // 1500 pixels wide and auto height
-    add_image_size( 'extra-large', 2500, 2500, true );
+	add_image_size( 'hero', 1500, 9999 ); // 1500 pixels wide and auto height
+	add_image_size( 'extra-large', 2500, 2500, true );
 }
 
 // Change the length of the excerpt
-function custom_excerpt_length($length) {
-    return 25;
+function custom_excerpt_length( $length ) {
+	return 25;
 }
-add_filter('excerpt_length', 'custom_excerpt_length');
+
+add_filter( 'excerpt_length', 'custom_excerpt_length' );
 
 // Add some custom footer text in the admin area
-function custom_admin_footer () {
-  echo 'This theme was developed by Alexander Persky.';
+function custom_admin_footer() {
+	echo 'This theme was developed by Alexander Persky.';
 }
-add_filter('admin_footer_text', 'custom_admin_footer');
+
+add_filter( 'admin_footer_text', 'custom_admin_footer' );
 
 // Update the default [...] with a custom link
-function replace_excerpt($content) {
-    return str_replace('[&hellip;]', '<a class="continue-reading" href="' . get_permalink() . '">Read More&hellip;</a>', $content);
+function replace_excerpt( $content ) {
+	return str_replace( '[&hellip;]', '<a class="continue-reading" href="' . get_permalink() . '">Read More&hellip;</a>', $content );
 }
-add_filter('the_excerpt', 'replace_excerpt');
+
+add_filter( 'the_excerpt', 'replace_excerpt' );
 
 // Blacklist WordPress menu class names
 // Review https://developer.wordpress.org/reference/functions/wp_nav_menu/#Menu_Item_CSS_Classes for a full class listing
-add_filter( 'nav_menu_css_class', function ( array $classes, $item, $args, $depth ) {
-    $disallowed_class_names = array(
-        "menu-item",
-        "menu-item-{$item->ID}",
-        "menu-item-type-{$item->type}",
-        "menu-item-object-{$item->object}",
-        "current-{$item->type}-item",
-        "current-{$item->object}-item",
-        "current-{$item->type}-parent",
-        "current-{$item->object}-parent",
-        "current-{$item->type}-ancestor",
-        "current-{$item->object}-ancestor",
-        'page_item',
-        'page_item_has_children',
-        "page-item-{$item->object_id}",
-        'current_page_item',
-        'current_page_parent',
-        'current_page_ancestor'
-    );
+add_filter( 'nav_menu_css_class', function( array $classes, $item, $args, $depth ) {
+	$disallowed_class_names = array(
+		"menu-item",
+		"menu-item-{$item->ID}",
+		"menu-item-type-{$item->type}",
+		"menu-item-object-{$item->object}",
+		"current-{$item->type}-item",
+		"current-{$item->object}-item",
+		"current-{$item->type}-parent",
+		"current-{$item->object}-parent",
+		"current-{$item->type}-ancestor",
+		"current-{$item->object}-ancestor",
+		'page_item',
+		'page_item_has_children',
+		"page-item-{$item->object_id}",
+		'current_page_item',
+		'current_page_parent',
+		'current_page_ancestor'
+	);
 
-    foreach ( $classes as $class ) {
-        if ( in_array( $class, $disallowed_class_names ) ) {
-            $key = array_search( $class, $classes );
+	foreach ( $classes as $class ) {
+		if ( in_array( $class, $disallowed_class_names ) ) {
+			$key = array_search( $class, $classes );
 
-            if ( false !== $key ) {
-                unset( $classes[ $key ] );
-            }
-        }
-    }
+			if ( false !== $key ) {
+				unset( $classes[ $key ] );
+			}
+		}
+	}
 
-    return $classes;
+	return $classes;
 }, 10, 4 );
 
 // Remove all the IDs from all menu items except for the ones listed.
-function my_css_attributes_filter($var) {
-    $whitelist = array();
+function my_css_attributes_filter( $var ) {
+	$whitelist = array();
 
-    return is_array($var) ? array_intersect($var, $whitelist) : '';
+	return is_array( $var ) ? array_intersect( $var, $whitelist ) : '';
 }
-add_filter('nav_menu_item_id', 'my_css_attributes_filter', 100, 1);
+
+add_filter( 'nav_menu_item_id', 'my_css_attributes_filter', 100, 1 );
