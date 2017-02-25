25e518523172c4ad4de8fe04b497e3ae3ed274ec
Alexander Persky <info@alexanderpersky.com>
2017-02-25 10:42:38 -0700

    Rename the functions folder to inc
    - this is a more common convention
    - prefixed all the files within with ‘theme-‘

--- functions.php
+++ functions.php
@@ -8,25 +8,25 @@
  */
 
 // Set up the theme
-require_once get_parent_theme_file_path('/functions/setup.php');
+require_once get_parent_theme_file_path('/inc/theme-setup.php');
 
 // Enqueue CSS and JS
-require_once get_parent_theme_file_path('/functions/enqueues.php');
+require_once get_parent_theme_file_path('/inc/theme-enqueues.php');
 
 // Register any widgets
-require_once get_parent_theme_file_path('/functions/widgets.php');
+require_once get_parent_theme_file_path('/inc/theme-widgets.php');
 
 // Custom Header implementation
-require_once get_parent_theme_file_path('/functions/custom-header.php');
+require_once get_parent_theme_file_path('/inc/theme-header.php');
 
 // Custom template tags for this theme
-require_once get_parent_theme_file_path('/functions/template-tags.php');
+require_once get_parent_theme_file_path('/inc/theme-tags.php');
 
 // Customizer implementation
-require_once get_parent_theme_file_path('/functions/customizer.php');
+require_once get_parent_theme_file_path('/inc/theme-customizer.php');
 
 // WordPress tweaks specifically for this theme
-require_once get_parent_theme_file_path('/functions/extras.php');
+require_once get_parent_theme_file_path('/inc/theme-extras.php');
 
 // Custom functions that act independently of the theme templates
-require_once get_parent_theme_file_path('/functions/custom-functions.php');
+require_once get_parent_theme_file_path('/inc/theme-functions.php');
