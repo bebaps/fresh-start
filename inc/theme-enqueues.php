c783ae7311f8f8d3d6f971ded58c72ec6d9b439a
Alexander Persky <info@alexanderpersky.com>
2017-02-25 12:34:33 -0700

    Update gulp tasks to be more streamlined
    - get rid of the root variable, its kind of pointless
    - don’t watch for HTML files anymore
    - don’t automatically concat jQuery anymore
    - minify CSS and JS
    - no longer a need for Sass options
    - other misc. changes

--- inc/theme-enqueues.php
+++ inc/theme-enqueues.php
@@ -4,9 +4,9 @@
  */
 function fresh_start_scripts()
 {
-  wp_enqueue_style('fresh-start-styles', get_theme_file_uri('/assets/css/theme.css'));
+  wp_enqueue_style('fresh-start-styles', get_theme_file_uri('/assets/css/theme.min.css'));
 
-  wp_enqueue_script('fresh-start-scripts', get_theme_file_uri('/assets/js/theme.js'), [], '', true);
+  wp_enqueue_script('fresh-start-scripts', get_theme_file_uri('/assets/js/theme.min.js'), [], '', true);
 
   if (is_singular() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
@@ -20,7 +20,7 @@ add_action('wp_enqueue_scripts', 'fresh_start_scripts');
  */
 function custom_login_stylesheet()
 {
-  wp_enqueue_style('custom-login', get_theme_file_uri('/assets/css/theme-login.css'));
+  wp_enqueue_style('custom-login', get_theme_file_uri('/assets/css/theme-login.min.css'));
 }
 
 add_action('login_enqueue_scripts', 'custom_login_stylesheet');
