96aa0377cb3f3a5db021835cb73a312c6f908f81
Alexander Persky <info@alexanderpersky.com>
2019-12-15 18:11:36 -0700

    Keep working on converting the Gulpfile to Gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,16 @@
 const localURL = 'sandbox.test';
 const sourceFiles = {
-  css: './src/css/**/*.scss',
-  js: './src/js/**/*.js',
   images: './src/images/**/*',
-  php: [
-    './*.php',
-    './**/*.php'
-  ]
+  php: './**/*.php'
 };
 const dist = {
-  css: './dist/css',
-  js: './dist/js',
   images: './dist/images'
 };
 const options = {
-  autoprefixer: {
-    browsers: ['last 2 versions'],
-    cascade: false
-  },
-  browsersync: {
-    proxy: localURL
-  },
   cssnano: {
     discardComments: {
       removeAll: true
     }
-  },
-  rename: {
-    css: {
-      suffix: '.min'
-    },
-    js: {
-      suffix: '.min'
-    }
-  },
-  sass: {
-    outputStyle: 'expanded'
-  },
-  sourcemaps: {
-    write: '/'
-  },
-  stylelint: {
-    reporters: [{
-      formatter: 'string',
-      console: true
-    }]
   }
 };
 
@@ -54,14 +20,13 @@ const browsersync = require('browser-sync').create();
 const cssnano = require('cssnano');
 const del = require('del');
 const babel = require('gulp-babel');
-const eslint = require('gulp-eslint');
 const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
 const postcss = require('gulp-postcss');
 const plumber = require('gulp-plumber');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
-const stylelint = require('gulp-stylelint');
 const uglify = require('gulp-uglify');
 sass.compiler = require('node-sass');
 
@@ -71,7 +36,9 @@ sass.compiler = require('node-sass');
 
 // BrowserSync
 function server() {
-  browsersync.init(options.browsersync);
+  browsersync.init({
+    proxy: localURL
+  });
 }
 
 // BrowserSync Reload
@@ -83,48 +50,48 @@ function serverReload() {
 // Sass Tasks
 // -----------------------------------------------------------------------------
 
-// Compile Sass
+// Compile Sass to CSS
 function css() {
-  return src(sourceFiles.css)
+  return src('src/css/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(plumber())
-    .pipe(sass(options.sass).on('error', sass.logError)).on('error', notify.onError('Error compiling Sass!'))
-    .pipe(dest(dist.css))
-    .pipe(rename(options.rename.css))
-    .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(sourcemaps.write(options.sourcemaps.write))
-    .pipe(dest(dist.css))
-    .pipe(browsersync.stream())
+    .pipe(sass().on('error', sass.logError)).on('error', notify.onError('Error compiling Sass!'))
+    .pipe(sourcemaps.write())
+    .pipe(dest('dist/css'))
+    .pipe(browsersync.stream());
 }
 
-// Lint Sass via Stylelint
-function lintCss() {
-  return src(paths.css.scss)
+// Minify CSS
+function optimizeCss() {
+  return src('dist/css/**/*.css')
     .pipe(plumber())
-    .pipe(stylelint(options.stylelint));
+    .pipe(rename({suffix: '.min'}))
+    .pipe(postcss([autoprefixer(), cssnano(options.cssnano)]))
+    .pipe(dest('dist/css'))
+    .pipe(browsersync.stream());
 }
 
 // -----------------------------------------------------------------------------
 // JS Tasks
 // -----------------------------------------------------------------------------
 
-// Lint JS via ESLint
-function lintJs() {
-  return src('./assets/js/theme/**/*.js')
-    .pipe(plumber())
-    .pipe(eslint())
-    .pipe(eslint.format());
-}
-
-// Concatenate and minify JS, then create a sourcemap
+// Transpile ES6
 function js() {
-  return src(paths.js.concat)
+  return src('src/js/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(plumber())
+    .pipe(babel())
+    .pipe(sourcemaps.write())
+    .pipe(dest('dist/js'))
+    .pipe(browsersync.stream());
+}
+
+function optimizeJs() {
+  return src('dist/js/**/*.js')
+    .pipe(plumber())
+    .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
-    .pipe(rename(options.rename.js))
-    .pipe(sourcemaps.write(options.sourcemaps.write))
-    .pipe(gulp.dest(paths.js.dest))
+    .pipe(dest('dist/js'))
     .pipe(browsersync.stream());
 }
 
@@ -149,7 +116,9 @@ function watchFiles() {
 }
 
 exports.css = css;
+exports.optimizeCss = optimizeCss;
 exports.js = js;
+exports.optimizeJs = optimizeJs;
 exports.server = server;
 exports.watchFiles = watchFiles;
 exports.default = series(css, server, watchFiles);
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+console.log('Ready to use...');
+
+jQuery(document).ready(function ($) {
+  const stuff = (thing) => alert(thing);
+});
